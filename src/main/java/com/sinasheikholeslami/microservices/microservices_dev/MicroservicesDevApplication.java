8603ce75387fc612ced8f8bd501db9fa22ed973d
sinasheikholeslami <sina@sinasheikholeslami.com>
2024-07-22 20:23:12 -0700

    initial commit

---
+++ src/main/java/com/sinasheikholeslami/microservices/microservices_dev/MicroservicesDevApplication.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.microservices.microservices_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinasheikholeslami/microservices/microservices_dev/MicroservicesDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinasheikholeslami.microservices.microservices_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
